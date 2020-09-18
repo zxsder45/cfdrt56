992c338c77ee711e7fa6eec5aa6703a30a27d89f
Sun Wenjie <sunwenjie8888@outlook.com>
2020-09-18 08:20:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# VLESS Heroku
+
+## 概述
+
+用于在 Heroku 上部署 vless。
+vless 性能更加优秀，占用资源更少。
+
+## 镜像
+
+经测试本镜像不会因为大量占用资源而被封号。
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://dashboard.heroku.com/new?template=https%3A%2F%2Fgithub.com%2FGeekNAUer%2Fvlessheroku)
+
+## ENV 设定
+
+### UUID
+
+`UUID` > `一个 UUID，供用户连接时验证身份使用`。
+
+## 注意
+
+WebSocket 路径为 `/app`。
+
+`alterId` 为 `0`。
---
+++ configure.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Download and install V2Ray
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+  "log": {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": $PORT,
+      "tag": "VLESS-in",
+      "protocol": "VLESS",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 0
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/app"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "dns": {
+    "servers": [
+      "https+local://1.1.1.1/dns-query",
+      "1.1.1.1",
+      "1.0.0.1",
+      "8.8.8.8",
+      "8.8.4.4",
+      "localhost"
+    ]
+  },
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [
+      {
+        "type": "field",
+        "inboundTag": [
+          "VLESS-in"
+        ],
+        "outboundTag": "direct"
+      }
+    ]
+  }
+}
+EOF
+
+# Run V2Ray
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
